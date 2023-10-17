204b8c34bb9c8c422073f868fc3ab486370b5194
juanwark <142903745+juanwark@users.noreply.github.com>
2023-10-17 12:19:17 -0500

    Mascota.java

--- Mascota.java
+++ Mascota.java
@@ -1,12 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package modelo;
 
 /**
  *
- * @author Usuario
+ * @author juan david
  */
 public class Mascota {
     protected String nombre;
